6ff35d1d4441310cdb179f35f76dede491dcf937
sdurante <sdurante@leadtech.com>
2021-10-05 15:50:11 +0200

    second commit

---
+++ login-udemy/src/App.js
@@ -0,0 +1,33 @@
+import {useState} from "react";
+
+function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailChange = ({target: { value }}) => setEmail(value);
+  const handlePasswordChange = ({ target: { value } }) => setPassword(value);
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    alert(`Usuario: ${email}, Password: ${password}`);
+  }
+
+  return (
+    <div className="App">
+      <form onSubmit={ handleFormSubmit }>
+        <h2>Iniciar sesión</h2>
+        <label>
+          Correo
+          <input type="email" value={email} onChange={handleEmailChange}/>
+        </label>
+        <label>
+          Password
+          <input type="password" value={password} onChange={handlePasswordChange}/>
+        </label>
+        <button type="submit">Enter</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
